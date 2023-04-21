35d66bf67b0b240582188479a7bb85e387bd0f0c
geoprv <prohorovgosa@gmail.com>
2023-04-21 20:14:55 +0300

    styles for modified buffer's dots

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -3,6 +3,18 @@ require('bufferline').setup {
     diagnostics = "nvim_lsp",
     tab_size = 14,
   },
+
+  highlights = {
+    modified = {
+        fg = '#45ba65',
+    },
+    modified_visible = {
+        fg = '#45ba65',
+    },
+    modified_selected = {
+        fg = '#45ba65',
+    },
+  }
 }
 
 local map = vim.api.nvim_set_keymap
